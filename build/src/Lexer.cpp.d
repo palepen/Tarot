19b73a1058b823a81f6f67d9e6fcf7b1f23881e9
palepen <122384892+palepen@users.noreply.github.com>
2024-08-16 21:33:46 +0530

    refactor

--- build/src/Lexer.cpp.d
+++
@@ -1,7 +0,0 @@
-build/./src/Lexer.cpp.o: src/Lexer.cpp src/../includes/Lexer.h \
- src/../includes/Token.h src/../includes/TokenType.h \
- src/../includes/Source.h
-src/../includes/Lexer.h:
-src/../includes/Token.h:
-src/../includes/TokenType.h:
-src/../includes/Source.h:
